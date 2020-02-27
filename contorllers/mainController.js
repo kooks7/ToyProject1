292a7332c6080cc152c107de1249ff70ad045dec
kooks7 <busanminjae@naver.com>
2020-02-27 10:18:09 +0900

    Add Posting Time

--- contorllers/mainController.js
+++ contorllers/mainController.js
@@ -5,14 +5,25 @@ exports.main = (req, res, next) => {
     .then(content => {
       // 시간 계산 예쁘게 꾸미기~~~
       const nowTime = new Date().getTime();
-
+      let time;
+      let day;
+      let hour;
+      let min;
       for (i = 0; i < content.length; i++) {
-        let time = Math.floor(
-          (nowTime - content[i].time.getTime()) / 1000 / 60
-        );
-        content[i].time1 = time;
-      }
+        time = Math.floor((nowTime - content[i].time.getTime()) / 1000 / 60);
 
+        if (time > 24 * 60) {
+          day = Math.floor(time / 24 / 60);
+          content[i].time1 = `${day}일`;
+        } else {
+          if (time > 60) {
+            hour = Math.floor(time / 60);
+            content[i].time1 = `${hour}시간`;
+          } else {
+            content[i].time1 = `${time}분`;
+          }
+        }
+      }
       res.render('main', {
         content: content
       });
